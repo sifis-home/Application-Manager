5e62619672cdbcf72767447ddcd86297b2b143a5
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-08-19 14:46:58 +0200

    Implement list_container

--- catch_topic.py
+++ catch_topic.py
@@ -5,10 +5,14 @@ import websocket
 import app_dht
 
 
-def handle_pull_image(topic_name):
+def handle_pull_image(ws, topic_name, topic_uuid):
     try:
         image_name = topic_name["image_name"]
-        result = app_dht.pull_image(image_name)
+        requestor_id = topic_name["requestor_id"]
+        request_id = topic_name["request_id"]
+        result = app_dht.pull_image(
+            ws, image_name, topic_uuid, requestor_id, request_id
+        )
         print(result)
     except Exception as e:
         print(e)
@@ -23,19 +27,27 @@ def handle_start_container(topic_name):
         print(e)
 
 
-def handle_remove_image(topic_name):
+def handle_remove_image(topic_name, topic_uuid):
     try:
         image_name = topic_name["image_name"]
-        result = app_dht.remove_image(image_name)
+        request_id = topic_name["request_id"]
+        requestor_id = topic_name["requestor_id"]
+        result = app_dht.remove_image(
+            image_name, topic_uuid, request_id, requestor_id
+        )
         print(result)
     except Exception as e:
         print(e)
 
 
-def handle_stop_container(topic_name):
+def handle_stop_container(topic_name, topic_uuid):
     try:
         container_id = topic_name["container_id"]
-        result = app_dht.stop_container(container_id)
+        request_id = topic_name["request_id"]
+        requestor_id = topic_name["requestor_id"]
+        result = app_dht.stop_container(
+            container_id, topic_uuid, request_id, requestor_id
+        )
         print(result)
     except Exception as e:
         print(e)
@@ -50,44 +62,45 @@ def handle_remove_container(topic_name):
         print(e)
 
 
-def handle_list_containers():
+def handle_list_containers(topic_uuid, requestor_id, request_id):
     try:
-        result = app_dht.list_containers()
+        result = app_dht.list_containers(
+            topic_uuid, requestor_id, request_id, None
+        )
         print(result)
     except Exception as e:
         print(e)
 
 
 def on_message(ws, message):
-    print("Received:")
-
     json_message = json.loads(message)
 
     if "Persistent" in json_message:
         json_message = json_message["Persistent"]
-
-        print("JSON-MESSAGE")
-        print(json_message["topic_name"])
-
         # Handle messages
         topic_name = json_message["topic_name"]
+        topic_uuid = json_message["topic_uuid"]
         if topic_name == "SIFIS:app_manager":
             if "value" in json_message:
                 topic_value = json_message["value"]
+                request_id = topic_value["request_id"]
+                requestor_id = topic_value["requestor_id"]
                 if "operation" in topic_value:
                     operation = topic_value["operation"]
                     if operation == "pull_image":
-                        handle_pull_image(topic_value)
+                        handle_pull_image(ws, topic_value, topic_uuid)
                     elif operation == "remove_image":
-                        handle_remove_image(topic_value)
+                        handle_remove_image(topic_value, topic_uuid)
                     elif operation == "start_container":
                         handle_start_container(topic_value)
                     elif operation == "stop_container":
-                        handle_stop_container(topic_value)
+                        handle_stop_container(topic_value, topic_uuid)
                     elif operation == "remove_container":
                         handle_remove_container(topic_value)
                     elif operation == "list_containers":
-                        handle_list_containers()
+                        handle_list_containers(
+                            topic_uuid, requestor_id, request_id
+                        )
 
 
 def on_error(ws, error):

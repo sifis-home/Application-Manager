8403cc310a6b0e53165862f3ed9d99ee76133c86
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-08-21 10:44:48 +0200

    Add yggio communication

--- app_dht.py
+++ app_dht.py
@@ -1,7 +1,11 @@
+import docker
+
+import websocket
+
 import json
 
-import docker
 import requests
+
 
 client = docker.from_env()
 
@@ -10,20 +14,19 @@ api_url = "http://localhost:3000/"
 
 def publish(ws, topic_uuid, requestor_id, request_id, containers):
     ws_req = {
-        "RequestPostTopicUUID": {
+            "RequestPostTopicUUID": {
             "topic_name": "SIFIS:container_list",
             "topic_uuid": topic_uuid,
             "value": {
                 "requestor_id": requestor_id,
                 "request_id": request_id,
-                "containers": containers,
-            },
+                "containers": containers
+                }
+            }
         }
-    }
     ws.send(json.dumps(ws_req))
     print("[!] Container List has been updated")
     return
-
 
 def request_list(ws, message, image_name):
     json_message = json.loads(message)
@@ -38,7 +41,8 @@ def request_list(ws, message, image_name):
             containers = topic_value["containers"]
             containers.append(image_name)
             publish(ws, topic_uuid, requestor_id, request_id, containers)
-            print("Active Containers: " + str(containers))
+            print('Active Containers: ' + str(containers))
+
 
 
 def update_dht_list(ws, image_name):
@@ -54,46 +58,31 @@ def update_dht_list(ws, image_name):
         publish(ws, topic_uuid, requestor_id, request_id, containers)
     else:
         request_list(ws, message, image_name)
-
-
+    
 def pull_image(ws, image_name, topic_uuid, requestor_id, request_id):
     if image_name:
         try:
             client.images.pull(image_name)
-            update_dht_list(ws, image_name)
+            update_dht_list(ws,image_name)
             topic_name = "SIFIS:application_manager_pull_image"
             pulling_data = {
                 "requestor_id": requestor_id,
                 "request_id": request_id,
-                "pulled_image": image_name,
-                "operation": "pull image",
-                "result": "successfull",
+                "pulled_image": image_name, 
+                "operation" : "pull image", 
+                "result" : "successfull",
             }
-            requests.post(
-                api_url
-                + "topic_name/"
-                + topic_name
-                + "/topic_uuid/"
-                + topic_uuid,
-                json=pulling_data,
-            )
+            requests.post(api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=pulling_data)
             return f"Image {image_name} pulled successfully!"
         except docker.errors.APIError as e:
             pulling_data = {
                 "requestor_id": requestor_id,
                 "request_id": request_id,
-                "pulled_image": image_name,
-                "operation": "pull image",
-                "result": "Error while pulling image {image_name}: {e}",
+                "pulled_image": image_name, 
+                "operation" : "pull image", 
+                "result" : "Error while pulling image {image_name}: {e}",
             }
-            requests.post(
-                api_url
-                + "topic_name/"
-                + topic_name
-                + "/topic_uuid/"
-                + topic_uuid,
-                json=pulling_data,
-            )
+            requests.post(api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=pulling_data)
             return f"Error while pulling image {image_name}: {e}", 500
     else:
         return "Missing 'image_name' parameter", 400
@@ -124,18 +113,11 @@ def stop_container(container_id, topic_uuid, request_id, requestor_id):
             container_info = {
                 "requestor_id": requestor_id,
                 "request_id": request_id,
-                "container_id": container_id,
-                "operation": "stop container",
-                "result": "successfull",
+                "container_id": container_id, 
+                "operation" : "stop container", 
+                "result" : "successfull",
             }
-            requests.post(
-                api_url
-                + "topic_name/"
-                + topic_name
-                + "/topic_uuid/"
-                + topic_uuid,
-                json=container_info,
-            )
+            requests.post(api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=container_info)
             return f"Container {container_id} stopped successfully!"
         except docker.errors.NotFound as e:
             return f"Container {container_id} not found: {e}", 404
@@ -169,14 +151,11 @@ def remove_image(image_name, topic_uuid, request_id, requestor_id):
         removing_info = {
             "requestor_id": requestor_id,
             "request_id": request_id,
-            "image": image_name,
-            "operation": "remove image",
-            "result": "successfull",
+            "image": image_name, 
+            "operation" : "remove image", 
+            "result" : "successfull",
         }
-        requests.post(
-            api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid,
-            json=removing_info,
-        )
+        requests.post(api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=removing_info)
         list_containers(topic_uuid, requestor_id, request_id, image_name)
         return f"Image {image_name} removed successfully!"
     except docker.errors.ImageNotFound as e:
@@ -189,7 +168,7 @@ def list_containers(topic_uuid, requestor_id, request_id, image_name):
     topic_name = "SIFIS:container_list"
     response = requests.get(api_url + "topic_name/" + topic_name)
     message = str(response.json()[0])
-    message = message.replace("'", '"')
+    message = message.replace("'", '"')   
     if "value" in str(message):
         json_message = json.loads(message)
         topic_value = json_message["value"]
@@ -199,10 +178,10 @@ def list_containers(topic_uuid, requestor_id, request_id, image_name):
         _list = {
             "requestor_id": requestor_id,
             "request_id": request_id,
-            "containers": containers,
-        }
-    response = requests.post(
-        api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid,
-        json=_list,
-    )
+            "containers": containers
+            }
+    local_response = requests.post(api_url + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=_list)
+    remote_host = "https://yggio.sifis-home.eu:3000/dht-insecure/"
+    yggio_response = requests.post(remote_host + "topic_name/" + topic_name + "/topic_uuid/" + topic_uuid, json=_list, verify=False)
+    print('YGGIO Response: ' + str(yggio_response.content) + '\n\n')
     return _list
